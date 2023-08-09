9dafda4ab4815d77f28fb4a8839e1fdaa334c5c1
afansyarifudin <afan.syarifudin10@gmail.com>
2023-08-09 23:30:54 +0700

    Update Category Index

---
+++ app/Models/Category.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Category extends Model
+{
+    use HasFactory;
+
+    public function childern() {
+        return $this->hasMany(self::class, 'parent_id');
+    }
+
+    public function descendants() {
+        return $this->childern()->with('descendants');
+    }
+}
